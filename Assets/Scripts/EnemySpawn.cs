df525c0b0257b4d512c1d0db62b3e3c2a7f84d61
DavidJan06 <david.jan06@gmail.com>
2023-02-05 01:33:36 +0100

    EnemySpawnFix

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    int Clock;
-
     [SerializeField]
-    float range = 1;
-    float buffer = 20;
+    float range = 100f;
 
     [SerializeField]
     List<GameObject> Vegans = new List<GameObject>();
@@ -24,60 +21,47 @@ public class EnemySpawn : MonoBehaviour
 
     float Timer = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Clock = (int)FindObjectOfType<GameManagement>().Clock;
-    }
+    int NoramlWave = 30;
+    int DeathWave = 10;
+    bool DWTrigger = false;
+    float WaveSec = 0;
+    float SpawnRateTemp = 0.5f;
+    
+
     private void Update()
     {
-        //*InvokeRepeating("Spawn", 0, SpawnRate);
-
         Timer += Time.deltaTime;
+        WaveSec += Time.deltaTime;
 
-        if(Timer >= SpawnRate)
+        if (Timer >= SpawnRate)
         {
             Spawn();
             Timer -= SpawnRate;
         }
-
-        switch (Clock)
+        
+        if (!DWTrigger)
         {
-            case > 150: // Death Wave
-                Amount = 300;
+            if (!((int)WaveSec <= NoramlWave)) //Normal Wave
+            {
+                WaveSec -= NoramlWave;
+                Amount += 50;
                 SpawnRate = 0.1f;
-                break;
-            case > 130 and <= 150:
-                Amount = 150;
-                SpawnRate = 0.3f;
-                break;
-            case > 120 and <= 130: // 3. Wave
-                Amount = 175;
-                SpawnRate = 0.1f;
-                break;
-            case > 80 and <= 120:
-                Amount = 100;
-                SpawnRate = 0.3f;
-                break;
-            case > 70 and <= 80: // 2. Wave
-                Amount = 125;
-                SpawnRate = 0.1f;
-                break;
-            case > 40 and <= 70:
-                Amount = 75;
-                SpawnRate = 0.4f;
-                break;
-            case > 30 and <= 40: // 1. Wave
-                SpawnRate = 0.1f;
-                Amount = 100;
-                Debug.Log("2");
-                break;
-            case <= 30:
-                SpawnRate = 0.5f;
-                Amount = 50;
-                Debug.Log("1");
-                break;
+                DWTrigger= true;
+            }
         }
+        else
+        {
+            if (!((int)WaveSec <= DeathWave)) //Death Wave
+            {
+                WaveSec -= DeathWave;
+                Amount -= 25;
+                range += (range < 100)? 10: 0;
+                SpawnRateTemp += (SpawnRateTemp == 0.1f)? 0.0f:-0.1f;
+                DWTrigger = false;
+            }
+        }
+
+        
     }
 
     // Update is called once per frame
@@ -85,6 +69,11 @@ public class EnemySpawn : MonoBehaviour
     {
         //Debug.Log(container.childCount);
         if (container.childCount < Amount)
-            Instantiate(Vegans[Random.Range(0, Vegans.Count)], transform.position + new Vector3(Random.Range(-range, range) * buffer, Random.Range(-range, range) * buffer, 0.0f), transform.rotation, container);
+        {
+            float Angle = Random.Range(0, 360);
+            float Rads = Mathf.Deg2Rad * Angle;
+            Vector2 Direction = new Vector2(Mathf.Cos(Rads), Mathf.Sin(Rads));
+            Instantiate(Vegans[Random.Range(0, Vegans.Count)], transform.position + new Vector3(Direction.x * range, Direction.y * range, 0.0f), transform.rotation, container);
+        }
     }
 }
