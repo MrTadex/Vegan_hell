81c538675b0b1bfc62739282916b67b0747ab99e
DavidJan06 <david.jan06@gmail.com>
2023-02-05 14:42:50 +0100

    Build Fix

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using static UnityEngine.UI.Image;
 
@@ -36,10 +37,54 @@ public class PickUp : MonoBehaviour
     private void ChangePlayer(GameObject oldPlayer)
     {
         GameObject newPlayer = Instantiate(players[type], oldPlayer.transform.position, oldPlayer.transform.rotation);
-        UnityEditorInternal.ComponentUtility.CopyComponent(oldPlayer.GetComponent<EnemySpawn>());
-        Destroy(newPlayer.GetComponent<EnemySpawn>());
-        UnityEditorInternal.ComponentUtility.PasteComponentAsNew(newPlayer);
+
+        newPlayer.GetComponent<EnemySpawn>().GetCopyOf(oldPlayer.GetComponent<EnemySpawn>());
+        
         Camera.main.GetComponent<CameraFollow>().target = newPlayer;
         Destroy(oldPlayer);
+    }
+}
+
+public static class ComponentExtensions
+{
+    public static T GetCopyOf<T>(this T comp, T other) where T : Component
+    {
+        System.Type type = comp.GetType();
+        System.Type othersType = other.GetType();
+        if (type != othersType)
+        {
+            Debug.LogError($"The type \"{type.AssemblyQualifiedName}\" of \"{comp}\" does not match the type \"{othersType.AssemblyQualifiedName}\" of \"{other}\"!");
+            return null;
+        }
+
+        BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default;
+        PropertyInfo[] pinfos = type.GetProperties(flags);
+
+        foreach (var pinfo in pinfos)
+        {
+            if (pinfo.CanWrite)
+            {
+                try
+                {
+                    pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+                }
+                catch
+                {
+                    /*
+                     * In case of NotImplementedException being thrown.
+                     * For some reason specifying that exception didn't seem to catch it,
+                     * so I didn't catch anything specific.
+                     */
+                }
+            }
+        }
+
+        FieldInfo[] finfos = type.GetFields(flags);
+
+        foreach (var finfo in finfos)
+        {
+            finfo.SetValue(comp, finfo.GetValue(other));
+        }
+        return comp as T;
     }
 }
